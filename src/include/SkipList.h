44420136d8a86aedbe4a74c28f3eaa9cbc0727b0
BustDot <Bust.dev@outlook.com>
2024-04-02 15:26:28 +0800

    fix: Template implementation can't write in a seperated file

--- src/include/SkipList.h
+++ src/include/SkipList.h
@@ -1,5 +1,6 @@
 #ifndef SKIPLIST_BSKIPLIST_H
 #define SKIPLIST_BSKIPLIST_H
+#include <iostream>
 #include <memory>
 #include <mutex>
 #include <random>
@@ -15,6 +16,8 @@ class Node {
   K GetKey() const { return key_; }
 
   V GetValue() const { return value_; }
+
+  void SetValue(V value) { value_ = value; }
 
   /** Store the pointers to the next nodes in each level */
   std::vector<std::shared_ptr<Node<K, V>>> forward_;
@@ -44,6 +47,7 @@ class SkipList {
   void remove(K key);
   bool exist(K key);
   int size() const;
+  void Print() const;
 
  private:
   int getRandLevel();
@@ -58,6 +62,82 @@ class SkipList {
   std::mt19937 mt_;
   std::uniform_int_distribution<int> dis_;
 };
+
+/** Template implementation can't write in a seperated file */
+
+template <typename K, typename V>
+void SkipList<K, V>::insert(K key, V value) {
+  std::scoped_lock lock(mutex_);
+  auto cur = header_;
+  NodeVec<K, V> update(max_level_, nullptr);
+
+  for (int i = curr_level_; i >= 0; i--) {
+    while (cur->forward_[i] != nullptr && cur->forward_[i]->GetKey() < key) {
+      cur = cur->forward_[i];
+    }
+    update[i] = cur;
+  }
+  // Reach the bottom level
+  cur = cur->forward_[0];
+
+  // If the key already exists, update the value
+  if (cur != nullptr && cur->GetKey() == key) {
+    cur->SetValue(value);
+    return;
+  }
+
+  int level = getRandLevel();
+  if (level > curr_level_) {
+    for (int i = curr_level_ + 1; i < level; i++) {
+      update[i] = header_;
+    }
+    curr_level_ = level;
+  }
+
+  auto insert_node = std::make_shared<Node<K, V>>(key, value, level);
+  for (int i = 0; i < level; i++) {
+    insert_node->forward_[i] = update[i]->forward_[i];
+    update[i]->forward_[i] = insert_node;
+  }
+  size_++;
+}
+
+template <typename K, typename V>
+void SkipList<K, V>::remove(K key) {}
+
+template <typename K, typename V>
+bool SkipList<K, V>::exist(K key) {
+  return false;
+}
+
+template <typename K, typename V>
+int SkipList<K, V>::size() const {
+  return size_;
+}
+
+template <typename K, typename V>
+void SkipList<K, V>::Print() const {
+  std::cout << "\n*****Skip List*****"
+            << "\n";
+  for (int i = 0; i <= curr_level_; i++) {
+    auto node = header_->forward_[i];
+    std::cout << "Level " << i << ": ";
+    while (node != nullptr) {
+      std::cout << node->GetKey() << ": " << node->GetValue() << "; ";
+      node = node->forward_[i];
+    }
+    std::cout << std::endl;
+  }
+}
+
+template <typename K, typename V>
+int SkipList<K, V>::getRandLevel() {
+  int level = 1;
+  while (dis_(mt_) < SKIPLIST_P * 0xFFFF && level < max_level_) {
+    level++;
+  }
+  return level;
+}
 
 }  // namespace BSkipList
 
